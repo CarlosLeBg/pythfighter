c87ea0f6ed4b26f892995f61f1aff10d900da634
CarlosLeBg <carlalbertlieval81@gmail.com>
2024-10-20 16:27:07 +0200

    Modif de Carlos

--- src/main.py
+++ src/main.py
@@ -1,21 +1,11 @@
-import os
-import sys
-import subprocess
+import pygame
 from game import Game
-
-try:
-    import pygame
-except ImportError:
-    print("Pygame non installé. Installation en cours...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pygame"])
-    import pygame
 
 def main():
     pygame.init()
     game = Game()
     game.run()
     pygame.quit()
-    print("Fermeture du jeu")
 
 if __name__ == "__main__":
     main()

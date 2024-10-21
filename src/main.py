ccc336805897b89af7015f1eec8a12278235fba1
CarlosLeBg <carlalbertlieval81@gmail.com>
2024-10-21 07:52:19 +0200

    Modif de Carlos

--- src/main.py
+++ src/main.py
@@ -2,11 +2,14 @@ import pygame
 from launcher import Launcher
 
 def main():
-    pygame.init()
-    pygame.font.init()
-    launcher = Launcher()
-    launcher.run()
-    pygame.quit()
+    try:
+        pygame.init()
+        launcher = Launcher()
+        launcher.run()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        pygame.quit()
 
 if __name__ == "__main__":
     main()

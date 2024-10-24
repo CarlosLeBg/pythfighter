fdfc793eae1e9c971431dbe164495a707b55fb9a
CarlosBG83 <carlalbertlieval81@gmail.com>
2024-10-24 15:27:14 +0200

    Update

--- launcher.py
+++ launcher.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-import subprocess
 
 def launch_other_script():
-    subprocess.Popen(["python", "C:/chemin/vers/ton_script.py"])
+    # Commande pour lancer un autre script Python
+    pass
 
 def looop():
     root = tk.Tk()
@@ -13,12 +13,15 @@ def looop():
 
     logo = tk.PhotoImage(file=r"C:\Users\carla\Downloads\pythfighter\src\assets\logo.png")
 
+    # Redimensionnement de l'image en la forçant dans une taille spécifique
+    logo = logo.subsample(2, 2)  # Redimensionne en divisant la taille de l'image par 2
+
     logom = tk.Label(root, image=logo)
-    logom.pack(pady=(10, 0))  # Met l'image en haut avec une légère marge
+    logom.pack(pady=(10, 0))
 
     launch_button = tk.Button(root, text="Lancer le jeu", command=launch_other_script,
                               font=("Arial", 16), bg="#333", fg="white", relief="groove", padx=20, pady=10)
-    launch_button.pack(pady=30)  # Beau bouton stylé avec marges ajustées
+    launch_button.pack(pady=30)
 
     root.mainloop()
 

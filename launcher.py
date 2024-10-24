1c1212a7b7ba70285b8fd05bbbb67220258a3b08
CarlosBG83 <carlalbertlieval81@gmail.com>
2024-10-24 15:24:15 +0200

    Update

--- launcher.py
+++ launcher.py
@@ -1,7 +1,17 @@
 import tkinter as tk
-from tkinter import *
 
 def looop():
     root = tk.Tk()
     root.title("Launcher du jeu PythFighter")
+
+    logo = tk.PhotoImage(file=r"C:\Users\carla\Downloads\pythfighter\src\assets\logo.png")
+
+    logom = tk.Label(root, image=logo)
+    logom.pack(padx=20, pady=20)
+
+    root.geometry("500x500")  # Taille fixe pour l'équilibre visuel
+    root.resizable(False, False)  # Désactiver le redimensionnement pour garder un design propre
+
     root.mainloop()
+
+looop()

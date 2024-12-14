f434a8f7bcd8ea05a0489c28333d89962a9765e1
FlitzzXP <minecraft.player.14.titi@gmail.com>
2024-12-14 15:23:25 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,33 @@
 import main
 from main import *
 import pygame 
+
+class Thunderstrike(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity= 5
+        self.image = pygame.image.load()
+        
 pygame.init()
 #generation de fenetre de jeu
 pygame.display.set_caption("Pythfighter")
-pygame.display.set_mode((1920, 1080))
+screen=pygame.display.set_mode((1920, 1080))
 
+#importation du fond du jeu
+background=pygame.image.load('assets/Pixilart_App.jpg')
 running = True
 
 while running:
+    
+    #appliquation du background du jeu
+    screen.blit(background, (0,0))
+    
+    #mettre à jour l'écran
+    pygame.display.flip()
+    
     # si le joueur ferme la fenetre
     for event in pygame.event.get():
         #fermeture de jeu

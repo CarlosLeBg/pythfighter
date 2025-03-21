8c83ec4bbb7acd9cbed2de9c52f0a92426650556
Benjou <yes@email.com>
2025-03-21 09:13:47 +0100

    ytesss

--- healthbar.py
+++ healthbar.py
@@ -5,7 +5,7 @@ pygame.init()
 # Paramètres de la fenêtre de jeu
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Jeu avec Manette")
+pygame.display.set_caption("PythFighter")
 
 # Classe du joueur
 class Player:
